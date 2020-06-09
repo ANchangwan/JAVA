233cc6baf38b0db9d1f58de17054dd7f4b09e48d
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-06-10 01:18:27 +0900

    Update NormalGugudan.java

--- NormalGugudan.java
+++ NormalGugudan.java
@@ -1,11 +1,11 @@
-package kunsan.¾ÈÃ¢¿Ï1501023;
+
 
 public class NormalGugudan {
 	public void InputGuGudan() {
 		int Start = 0;
 		int End = 0;
 		
-		System.out.println("5) 99´Ü ´Ü¼ø Âï±â");
+		System.out.println("5) 99Â´Ãœ Â´ÃœÂ¼Ã¸ Ã‚Ã¯Â±Ã¢");
 			
 		for(int i=1;i<=9;i++,System.out.println()) {
 			for(int j=1;j<=9;j++,System.out.print("\t")) {
